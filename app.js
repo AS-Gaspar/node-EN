c9c21ab08ce2f94f72d9aa7a243c55b4857670bf
Alexandre <airtontunni@gmail.com>
2025-02-14 17:08:07 -0300

    routing requests

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+    const url = req.url
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><tittle>Enter Message</tittle></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Enter</button></form><body>')
+        res.write('</html>')
+        return res.end()
+    }
     console.log(req.url, req.method ,req.headers)
-    // process.exit()
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
