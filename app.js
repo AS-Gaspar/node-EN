83845c5f547b7c8b3c46fef4fb25e3e42b59acfc
Alexandre <airtontunni@gmail.com>
2025-02-14 08:26:35 -0300

    send html text on port 3000

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method ,req.headers)
     // process.exit()
     res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Hello! I create my first server with node.js!</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(3000)
