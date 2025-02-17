804b235e635a30f6be8794343ac68f3a176fd028
Alexandre <airtontunni@gmail.com>
2025-02-17 16:45:57 -0300

    modified message.txt

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const server = http.createServer((req, res) => {
         })
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString()
-            console.log(parsedBody)
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
         })
         fs.writeFileSync('message.txt', "DUMMY")
         res.statusCode = 302
