4b03b671f05bee173231a9c4fb4fc8fe9c75883b
Alexandre <airtontunni@gmail.com>
2025-02-16 22:20:24 -0300

    sunday studies

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 // Arquivo que será executado em um cpc em um servidor no final
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     const url = req.url
+    const method = req.method
     if (url === '/') {
         res.write('<html>')
         res.write('<head><tittle>Enter Message</tittle></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Enter</button></form><body>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Enter</button></form><body>')
         res.write('</html>')
         return res.end()
     }
+
+    if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody)
+        })
+        fs.writeFileSync('message.txt', "DUMMY")
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+    }    
+
     console.log(req.url, req.method ,req.headers)
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
