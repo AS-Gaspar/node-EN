79e6aa9100b744e82b5094f52ce61591671f5fb8
Alexandre <airtontunni@gmail.com>
2025-03-02 20:26:55 -0300

    continuação dos estudos

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const requestHandler = (req, res) => {
 
     if (url === '/') {
         res.write('<html>')
-        res.write('<head><tittle>Enter Message</tittle></head>')
+        res.write('<head><title>Enter Message</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Enter</button></form><body>')
         res.write('</html>')
         return res.end()
