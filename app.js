825a30096ce3ab165a72ed3b74e3e1d456c54dc1
Alexandre <airtontunni@gmail.com>
2025-03-06 11:35:22 -0300

    estilizando um pouco o html]

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const http = require('http')
 const path = require('path')
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const rootDir = require('./util/path')
 
-const shopRoutes = require('./routes/shop')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
 const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const PORT = 3000
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(shopRoutes)
 app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
  //const server = http.createServer(app)
