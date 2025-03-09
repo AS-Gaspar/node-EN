334b601e350b1c66c5ecac8f1680e3dfbbd79ac9
Alexandre <airtontunni@gmail.com>
2025-03-08 21:31:38 -0300

    complete express basics

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http')
 const path = require('path')
 
 const express = require('express')
-const bodyParser = require('body-parser')
 
+const bodyParser = require('body-parser')
 const app = express()
 
 const adminRoutes = require('./routes/admin')
