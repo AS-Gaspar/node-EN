17f76ace6cc1b7d4125932dad72e4aa992ad8c85
Alexandre <airtontunni@gmail.com>
2025-03-10 17:15:05 -0300

    create array and save the product input

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const PORT = 3000
@@ -14,7 +14,7 @@ const PORT = 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
