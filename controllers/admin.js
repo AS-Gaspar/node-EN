363e61d75ea00b419fa0cc3fa863dd737dbbc0a2
Alexandre <airtontunni@gmail.com>
2025-04-10 13:37:04 -0300

    create the cart and edit product funtionalities

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,10 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product'
+        path: '/admin/add-product',
+        editing: false
     })
 }
 
@@ -17,12 +18,31 @@ exports.getProducts = (req, res) => {
     })
 }
 
-exports.postAddProduct = (req, res) => { 
+exports.getEditProduct = (req, res) => {
+    const editMode = req.query.edit
+    if (!editMode) {
+        return res.redirect('/')
+    }
+    const prodId = req.params.productId
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        })
+    })
+}
+
+exports.postAddProduct = (req, res) => {
     const title = req.body.title
     const imageURL = req.body.imageUrl
     const description = req.body.description
     const price = req.body.price
-    const product = new Product(title, imageURL, description, price) 
+    const product = new Product(title, imageURL, description, price)
     product.save()
     res.redirect('/')
 }
